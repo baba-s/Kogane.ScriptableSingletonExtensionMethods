88cb25c495d8e747337c93ac97602d218a452e13
baba-s <shotababa1206@gmail.com>
2022-11-30 20:18:26 +0900

    add

---
+++ Editor/ScriptableSingletonExtensionMethods.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace Kogane
+{
+    /// <summary>
+    /// ScriptableSingleton 型の拡張メソッド
+    /// </summary>
+    public static class ScriptableSingletonExtensionMethods
+    {
+        //================================================================================
+        // 関数(static)
+        //================================================================================
+        /// <summary>
+        /// protected な Save 関数を public で呼び出せるようにする拡張メソッド
+        /// </summary>
+        public static void Save<T>( this ScriptableSingleton<T> self, bool saveAsText ) where T : ScriptableObject
+        {
+            var type       = typeof( ScriptableSingleton<T> );
+            var methodInfo = type.GetMethod( "Save", BindingFlags.NonPublic | BindingFlags.Instance );
+
+            Assert.IsNotNull( methodInfo );
+
+            methodInfo.Invoke( self, new object[] { saveAsText } );
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Kogane.ScriptableSingletonExtensionMethods
+# Kogane Scriptable Singleton Extension Methods
+
+ScriptableSingleton の拡張メソッド
+
+## 使用例
+
+```cs
+// protected な Save 関数を public で呼び出せるようにする拡張メソッド
+scriptableSingleton.Save( true );
+```
